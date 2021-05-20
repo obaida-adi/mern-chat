c62b8734cd40f4febba8e3b28864be332b569787
O'Baida Adi <obaida.adi@hotmail.com>
2021-05-19 20:34:11 -0400

    ws uri

--- src/contexts/socketContext.js
+++ src/contexts/socketContext.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import io from 'socket.io-client';
-import api from '../api';
 
 const SocketContext = React.createContext();
 
 const SocketProvider = ({ children }) => {
-
-    const socket = io(api.rootUrl, { transports: ['websocket', 'polling'] });
+    const url = 'wss://warm-shelf-42022.herokuapp.com/';
+    const socket = io(url, { transports: ['websocket', 'polling'] });
 
     return (
         <SocketContext.Provider value={socket}>
