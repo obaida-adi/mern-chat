0b537ebcddb006225848e7144737812cbafc0fd0
O'Baida Adi <obaida.adi@hotmail.com>
2021-05-19 20:07:22 -0400

    cleanup footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,24 @@
-import { Container, Typography } from '@material-ui/core';
-import nameLogo from '../assets/images/name-logo.png';
+import { Container, Typography, IconButton } from '@material-ui/core';
+import LanguageIcon from '@material-ui/icons/Language';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import nameLogo from '../assets/images/name-logo.svg';
 
 const Footer = () => {
+
+
+
     return (
         <Container style={{ textAlign: 'center', marginTop: '2rem'}}>
             <Typography variant='body1'>Designed by O'Baida Adi.</Typography>
-            <img src={nameLogo} style={{ width: '1.5rem', marginTop: '1rem' }} alt="name logo"/>
+            <Container style={{ display: 'flex', justifyContent: 'center' }}>
+                <IconButton color="primary" size="medium" onClick={() => window.open('https://obaida.netlify.app/')} aria-label="portfolio">
+                    <LanguageIcon />
+                </IconButton>
+                <IconButton color="primary" size="medium" onClick={() => window.open('https://github.com/obaida-adi')} aria-label="github">
+                    <GitHubIcon />
+                </IconButton>
+            </Container>
+            <img src={nameLogo} style={{ width: '2rem', marginTop: '1rem' }} alt="name logo"/>
         </Container>
     );
 }
